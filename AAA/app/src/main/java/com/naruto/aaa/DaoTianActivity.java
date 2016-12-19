175af4da4eb9c8cdc8dd62a0b76c62a4c9b0f145
史强 <3282035164@qq.com>
2016-12-19 11:22:53 +0800

    岛田的acy

---
+++ AAA/app/src/main/java/com/naruto/aaa/DaoTianActivity.java
@@ -0,0 +1,16 @@
+package com.naruto.aaa;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DaoTianActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dao_tian);
+        /**
+         * 岛田的Activity
+         */
+    }
+}
